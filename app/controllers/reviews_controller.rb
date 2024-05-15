5c1ce33ed3cd7e35cabcbd7fe999b6bba828bf1e
Jeremy Rome <jeremy.rome93@gmail.com>
2024-05-15 16:47:21 +0200

    Challenge solved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,19 @@
 class ReviewsController < ApplicationController
 
-  def new
-    @review = Review.new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
+  # def new
+  #   @review = Review.new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  # end
 
   def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show', status: :unprocessable_entity
+    end
   end
 
   private
